ccec4495ed87613b5489c9ed2a0fbd6feeb25b04
Sam Wallace <sam@sammymayhem.com>
2022-04-28 09:54:45 -0700

    got notes to write to db and delete

--- routes/notes.js
+++ routes/notes.js
@@ -8,17 +8,17 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-notes.get('/:id', (req, res) => {
-    const notesId = req.params.id;
-    readFromFile('./db/db.json')
-        .then((data) => JSON.parse(data))
-        .then((json) => {
-            const result = json.filter((notes) => notes.id === notesId);
-            return result.length > 0
-                ? res.json(result)
-                : res.json('No notes with that ID');
-        });
-});
+// notes.get('/:id', (req, res) => {
+//     const notesId = req.params.id;
+//     readFromFile('./db/db.json')
+//         .then((data) => JSON.parse(data))
+//         .then((json) => {
+//             const result = json.filter((notes) => notes.id === notesId);
+//             return result.length > 0
+//                 ? res.json(result)
+//                 : res.json('No notes with that ID');
+//         });
+// });
 
 notes.delete('/:id', (req, res) => {
     const notesId = req.params.id;
@@ -41,7 +41,7 @@ notes.post('/', (req, res) => {
     const { title, text } = req.body;
 
     // If all the required properties are present
-    if (title && text && id) {
+    if (req.body) {
         // Variable for the object we will save
         const newNote = {
             title,
@@ -49,7 +49,7 @@ notes.post('/', (req, res) => {
             id: uuidv4(),
         };
 
-        readAndAppend(newNote, './db/db.json');
+        readAndAppend(newNote, '../db/db.json');
 
         const response = {
             status: 'success',
